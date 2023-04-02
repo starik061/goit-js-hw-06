648d11fd23193ed84c66db581eaee6cc78158728
starik061 <starik061@gmail.com>
2023-04-02 16:42:37 +0300

    Second task is done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+function createItem(item) {
+  const itemElement = document.createElement("li");
+  itemElement.textContent = item;
+  itemElement.textContent;
+  itemElement.classList.add("item");
+  return itemElement;
+}
+
+let ingredientsElements = ingredients.map(createItem);
+console.log("🚀 ~ ingredientsElements:", ingredientsElements);
+
+const ingredientsListElement = document.querySelector("#ingredients");
+ingredientsListElement.append(...ingredientsElements);
